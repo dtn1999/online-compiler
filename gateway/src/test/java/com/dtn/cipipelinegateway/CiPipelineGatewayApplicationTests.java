69d91c674b0eaedcd61601892de48155898cbce0
@danyngongang <danyngongang@gmail.com>
2021-09-03 03:20:34 +0200

    refactoring

---
+++ gateway/src/test/java/com/dtn/cipipelinegateway/CiPipelineGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtn.cipipelinegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipelineGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
